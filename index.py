bec9f28c650ae6ede06261439aaf4471591ba8b7
mujoshi1 <45493821+mujoshi1@users.noreply.github.com>
2022-07-07 18:17:42 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,3 @@
-import dash
-import dash_bootstrap_components as dbc
-
-external_stylesheets = [dbc.themes.CYBORG]
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets,title='ServiceNow Dashboard',
-                update_title='Loading...', suppress_callback_exceptions=True, 
-                url_base_pathname='/ServiceNowDashboard/',
-			meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
-                )
-server = app.server
-
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
